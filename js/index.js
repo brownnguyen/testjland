f5443d9b75de8b280823f51abec27adbfaa6500a
Brown Nguyễn <brownnguyen@users.noreply.github.com>
2020-06-24 20:23:30 +0700

    less_responsive

--- js/index.js
+++ js/index.js
@@ -5,3 +5,9 @@ let menuBurger = dom('.menu__burger');
 const clickBurger = function () {
     menuBurger.classList.toggle('show');
 }
+let contact = dom('.contact');
+let lineRight = dom('.line__info .line__content .right')
+const clickLine = function() {
+    contact.classList.toggle('show');
+    lineRight.classList.toggle('bg__color');
+}
