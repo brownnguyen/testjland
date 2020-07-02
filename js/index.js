91abf7cb90ed739e914e3deb81c2637fcde8be5c
unknown <brownnguyen268@gmail.com>
2020-07-02 17:34:50 +0700

    add

--- js/index.js
+++ js/index.js
@@ -17,22 +17,20 @@ const closeButton = function () {
     menuBurger.classList.remove('show');
 }
 let topHeader = dom(".header__top");
-window.addEventListener("scroll", function () {
-    if (window.scrollY > 60) {
+window.onscroll = function () {
+    if (window.pageYOffset > 60) {
         topHeader.classList.add("fixedTop")
     }
     else {
         topHeader.classList.remove("fixedTop")
     }
-})
-window.onscroll = function () {
     let stick0 = dom('.adv__card:nth-child(1)');
     let stick1 = dom('.adv__card:nth-child(2)');
     let stick2 = dom('.adv__card:nth-child(3)');
     let stick3 = dom('.adv__card:nth-child(4)');
     let stick4 = dom('.adv__card:nth-child(5)');
     let stick5 = dom('.content__right');
-    if (window.pageYOffset + 400> stick0.offsetTop) {
+    if (window.pageYOffset + 400 > stick0.offsetTop) {
         odometer0.innerHTML = 8888;
     }
     if (window.pageYOffset + 400 > stick1.offsetTop) {
@@ -53,7 +51,7 @@ window.onscroll = function () {
 }
 $('#backToTop').click(function () {
     $('html,body').animate({
-      scrollTop: 0
+        scrollTop: 0
     }, 1000, 'swing');
     return false;
-  });
+});
